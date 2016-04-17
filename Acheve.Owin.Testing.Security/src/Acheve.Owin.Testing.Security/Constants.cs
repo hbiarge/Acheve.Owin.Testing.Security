5834df6057ab5213fed1293760d16852c9595661
Hugo Biarge <hugo@arosbi.com>
2016-04-17 19:53:10 +0200

    Fix bug with header name

--- Acheve.Owin.Testing.Security/src/Acheve.Owin.Testing.Security/Constants.cs
+++ Acheve.Owin.Testing.Security/src/Acheve.Owin.Testing.Security/Constants.cs
@@ -2,6 +2,6 @@
 {
     internal static class Constants
     {
-        public const string AuthenticationHeaderName = "Authentication";
+        public const string AuthenticationHeaderName = "Authorization";
     }
 }
